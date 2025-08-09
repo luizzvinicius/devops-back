f2ca8a2185e8a3771e0ac3573196d3879da8d5fe
luiz <viniciusadr244@gmail.com>
2025-08-09 12:55:56 -0300

    refactor: remove numeroConta from ContaRequestDto

--- src/main/java/com/luiz/devops/dtos/conta/ContaRequestDto.java
+++ src/main/java/com/luiz/devops/dtos/conta/ContaRequestDto.java
@@ -1,12 +1,8 @@
 package com.luiz.devops.dtos.conta;
 
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
-import java.util.UUID;
-
 public record ContaRequestDto(
-        @Positive Long pessoaId,
-        @NotNull UUID numeroConta
+        @Positive Long pessoaId
 ) {
 }
