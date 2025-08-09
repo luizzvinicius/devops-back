77d9637ea099da5ba937c346900ec4a6f8e6a251
luiz <viniciusadr244@gmail.com>
2025-08-09 12:52:00 -0300

    feat: create PessoaMapper

---
+++ src/main/java/com/luiz/devops/dtos/pessoa/PessoaMapper.java
@@ -0,0 +1,33 @@
+package com.luiz.devops.dtos.pessoa;
+
+import com.luiz.devops.dtos.conta.ContaMapper;
+import com.luiz.devops.dtos.conta.ContaResponseDto;
+import com.luiz.devops.models.Pessoa;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.List;
+
+@Component
+public class PessoaMapper {
+    private final ContaMapper contaMapper;
+
+    public PessoaMapper(ContaMapper contaMapper) {
+        this.contaMapper = contaMapper;
+    }
+
+    public PessoaResponseDto toDto(Pessoa pessoa) {
+        List<ContaResponseDto> contas = Collections.emptyList();
+        if (pessoa.getContas() != null) {
+            contas = pessoa.getContas().stream().map(contaMapper::toDto).toList();
+        }
+
+        return new PessoaResponseDto(
+                pessoa.getId(),
+                pessoa.getNome(),
+                pessoa.getCpf(),
+                pessoa.getEndereco(),
+                contas
+        );
+    }
+}
