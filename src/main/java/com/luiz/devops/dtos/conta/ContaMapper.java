3d30a03944f7c8c634317b2e55d076594f2beadf
luiz <viniciusadr244@gmail.com>
2025-08-09 12:52:48 -0300

    refactor: adapt ContaMapper to avoid nullPointer in movimentacoes

--- src/main/java/com/luiz/devops/dtos/conta/ContaMapper.java
+++ src/main/java/com/luiz/devops/dtos/conta/ContaMapper.java
@@ -1,14 +1,29 @@
 package com.luiz.devops.dtos.conta;
 
+import com.luiz.devops.dtos.movimentacoes.MovimentacoesMapper;
+import com.luiz.devops.dtos.movimentacoes.MovimentacoesResponseDto;
 import com.luiz.devops.models.Conta;
 import org.springframework.stereotype.Component;
 
+import java.util.Collections;
+import java.util.List;
+
 @Component
 public class ContaMapper {
+    private final MovimentacoesMapper movimentacoesMapper;
+
+    public ContaMapper(MovimentacoesMapper movimentacoesMapper) {
+        this.movimentacoesMapper = movimentacoesMapper;
+    }
+
     public ContaResponseDto toDto(Conta conta) {
+        List<MovimentacoesResponseDto> movimentacaoes = Collections.emptyList();
+        if (conta.getMovimentacoes() != null) {
+            movimentacaoes = conta.getMovimentacoes().stream().map(movimentacoesMapper::toDto).toList();
+        }
         return new ContaResponseDto(
                 conta.getId(),
-                conta.getMovimentacoes(),
+                movimentacaoes,
                 conta.getSaldo()
         );
     }
