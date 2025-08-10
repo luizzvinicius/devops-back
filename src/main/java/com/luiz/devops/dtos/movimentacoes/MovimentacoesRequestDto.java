0682eab08dc9bd5a864e4554bb3c091d37e0d6a9
luiz <viniciusadr244@gmail.com>
2025-08-10 11:27:00 -0300

    feat: add @Positive annotation in MovimentacoesRequestDto

--- src/main/java/com/luiz/devops/dtos/movimentacoes/MovimentacoesRequestDto.java
+++ src/main/java/com/luiz/devops/dtos/movimentacoes/MovimentacoesRequestDto.java
@@ -3,12 +3,13 @@ package com.luiz.devops.dtos.movimentacoes;
 import com.luiz.devops.enums.OperacaoEnum;
 import com.luiz.devops.enums.ValueOfEnum;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 import java.util.UUID;
 
 public record MovimentacoesRequestDto(
         @NotNull UUID contaId,
-        @NotNull double valor,
+        @NotNull @Positive double valor,
         @ValueOfEnum(enumClass = OperacaoEnum.class) String tipoMovimentacao
 ) {
 }
