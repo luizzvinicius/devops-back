d43ed534aa14ad6e87ad524e8d0620fbdc3b453f
luiz <viniciusadr244@gmail.com>
2025-08-26 20:46:29 -0300

    feat: validated annotation in Movimentacoes and pessoa controller

--- src/main/java/com/luiz/devops/controllers/MovimentacoesController.java
+++ src/main/java/com/luiz/devops/controllers/MovimentacoesController.java
@@ -6,8 +6,10 @@ import com.luiz.devops.services.MovimentacoesService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Validated
 @RestController
 @CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 @RequestMapping("/api/v1/movimentacoes")
--- src/main/java/com/luiz/devops/controllers/PessoaContoller.java
+++ src/main/java/com/luiz/devops/controllers/PessoaContoller.java
@@ -6,8 +6,10 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.PositiveOrZero;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Validated
 @RestController
 @CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 @RequestMapping("/api/v1/pessoa")
