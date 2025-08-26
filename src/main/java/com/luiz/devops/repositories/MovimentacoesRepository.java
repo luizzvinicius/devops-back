9c866dfd0a44824cd4908b4f235ad102bf934221
luiz <viniciusadr244@gmail.com>
2025-08-26 20:47:44 -0300

    feat: add repository annotation in PessoaRepository and MovimentacoesRepository

--- src/main/java/com/luiz/devops/repositories/MovimentacoesRepository.java
+++ src/main/java/com/luiz/devops/repositories/MovimentacoesRepository.java
@@ -2,6 +2,8 @@ package com.luiz.devops.repositories;
 
 import com.luiz.devops.models.Movimentacoes;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MovimentacoesRepository extends JpaRepository<Movimentacoes, Long> {
 }
